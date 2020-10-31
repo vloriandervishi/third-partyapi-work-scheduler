9376c3093e08aa0d29d88c31c494787130ecbf0e
Valdo <vlorian22@GMAIL.COM>
2020-10-30 22:16:14 -0500

    setup past present future

--- Develop/script/script.js
+++ Develop/script/script.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var currentDate = moment();
+    $('#currentDay').text(currentDate.format('dddd MM-DD-YYYY'));
 
+    $('.hour').each(function () {
+        var getHour = $(this).attr('id');
+        var currentHour = currentDate.hours();
+        var txtBx=$(this).siblings('.description');
+            
+        if (getHour === currentHour) {
+             txtBx.addClass('present');
+        }else if(getHour < currentHour)
+        {
+             txtBx.addClass('past');
+         }
+         else {
+             txtBx.addClass('future');
+         }
+
+        var toDo= localStorage.getItem(getHour,);
+         t
+        });
+
+        $('.saveBtn').on('click',()=>{
+          
+          var newBox=$(this).siblings('.description');
+          
+          localStorage.setItem('key',newBox.val());
+          
+          
+        });
 });
